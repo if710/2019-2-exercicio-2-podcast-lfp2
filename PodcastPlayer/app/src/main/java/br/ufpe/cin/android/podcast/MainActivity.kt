379b7d2a8a8d7dec66c460d2b5dfe883702f79eb
Lavínia Paganini <laviniapaganini@gmail.com>
2019-09-20 23:33:01 -0300

    Adding the Async Task to parse a XML from static URL.

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -2,11 +2,19 @@ package br.ufpe.cin.android.podcast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.jetbrains.anko.doAsync
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    doAsync{
+        val url = URL("https://audio.globoradio.globo.com/podcast/feed/711/rodada-tripla").readText()
+        Log.e("Titulo", Parser.parser(url).toString())
     }
 }
