d9a552ff367238ec7cbe31a9d8c9d36890fab25a
Lavínia Paganini <laviniapaganini@gmail.com>
2019-09-22 10:18:57 -0300

    Finishing the RecyclerView.

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/ItemFeedRecyclerView.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/ItemFeedRecyclerView.kt
@@ -9,11 +9,25 @@ class ItemFeedRecyclerView(private val listItemFeed: List<ItemFeed>) : Adapter<I
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
         // Inflates on the ItemLista layout.
         val view = LayoutInflater.from(context).inflate(R.layout.itemlista, parent, false)
-            return view
+            return ViewHolder(view)
+    }
+
+    // Returns the size of listItemFeed.
+    override fun getItemCount(): Int {
+        return listItemFeed.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        val itemFeed = listItemFeed[position]
+        holder?.let{
+            it.bindView(itemFeed)
         }
-        // Returns the size of listItemFeed.
-        override fun getItemCount(): Int {
-            return listItemFeed.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bindView(itemFeed: ItemFeed) {
+            val title = itemView.item_title
+            title.text = itemFeed.toString()
         }
+    }
 }
-class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){}
